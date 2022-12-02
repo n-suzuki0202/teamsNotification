efeae5419b8379d12748025712cdfac9acef9501
n-suzuki0202 <nobuhiro.suzuki@intloop.com>
2022-12-02 19:32:23 +0900

    画面遷移用url追加

--- index.js
+++ index.js
@@ -1,86 +1,102 @@
 const axios  = require('axios');
 
+const 
+  URI = 'teamsWebhookUrl',
+  BACKLOG_URI = 'https://xxx.backlog.com/';
+
 exports.handler = async event => {
   const 
-    uri = 'teamsURL',
     bl_body = JSON.parse(event.body),
-    bl_issue_type = { 1: "課題の追加", 2: "課題の更新", 3: "課題にコメント", 4: "課題の削除", 14: "課題をまとめて更新", 17: "コメントにお知らせを追加" },
     bl_type = bl_body.type;
 
-  let teams_title = '';
-  for (let key of Object.keys(bl_issue_type)) {
-    if (bl_type == key) {
-      teams_title = bl_issue_type[bl_type];
-    }
-  }
-
   let payload_dic = {};
-  if (bl_type == 1 || bl_type == 2) {
-    payload_dic = issue_add(teams_title, bl_body);
-  } else if (bl_type == 3) {
-    payload_dic = issue_comment(teams_title, bl_body);
-  } else if (bl_type == 4) {
-    payload_dic = issue_delete(teams_title, bl_body);
-  } else if (bl_type == 14) {
-    payload_dic = issue_multi_update(teams_title, bl_body);
-  } else if (bl_type == 17) {
-    payload_dic = issue_comment_notice_add(teams_title, bl_body);
+  if (bl_type === 14) {
+    payload_dic = issue_multi_update(bl_body);
+  } else {
+    payload_dic = handleIssue(bl_body);
   }
 
-  await axios.post(uri, payload_dic);
-  return 'success';
+  if (payload_dic !== null) {
+    await axios.post(URI, payload_dic);
+    return 'success';
+  }
+  return `unknown event type: ${bl_type}`;
 };
 
 
-// 課題add, update時のメッセージ作成
-const issue_add = (teams_title, bl_body) => {
-  const 
-    bl_summary = bl_body.content.summary,
-    bl_description = bl_body.content.description,
-    bl_created = bl_body.created;
-  
-  let teams_text = `件名：${bl_summary}<br/>課題の詳細：${bl_description}<br/>日時：${bl_created}`;
-  // Teamsへの通知本文作成
-  const payload_dic = {
-      "title": teams_title,
-      "text": teams_text,
-  };
+const create_title = body => {
+  const { createdUser, content } = body;
 
-  return payload_dic;
+  switch(body.type) {
+    case 1:
+      return `${createdUser.name}が「${content.summary}」を追加しました。`;
+    case 2:
+    case 3:
+      return `${createdUser.name}が「${content.summary}」を更新しました。\n${content.comment.content}`;
+    case 4:
+      return `${createdUser.name}が課題を削除しました。`;
+    case 6:
+      return `${createdUser.name}がWikiを更新しました。`;
+    case 14:
+      return `${createdUser.name}が課題をまとめて更新しました。`;
+    case 17:
+      return `${createdUser.name}がコメントにお知らせを追加しました。`;
+  }
+  return `unknown event type: ${body.type}`;
 }
 
-// 課題コメント時のメッセージ作成
-const issue_comment = (teams_title, bl_body) => {
-  const 
-    bl_summary = bl_body.content.summary,
-    bl_comment = bl_body.content.comment.content,
-    bl_created = bl_body.created;
-    
-  let teams_text = `件名：${bl_summary}<br/>コメント：${bl_comment}<br/>日時：${bl_created}`;
-  // Teamsへの通知本文
-  const payload_dic = {
-      "title": teams_title,
-      "text": teams_text,
-  };
-  return payload_dic;
+const create_text = body => {
+  const { content, created, type } = body;
+  const { summary, description, comment } = content;
+
+  switch(type) {
+    case 1:
+    case 2:
+      return `[課題に移動(WEBページに遷移)](${create_link(body)})<br/>件名：${summary}<br/>課題の詳細：${description}<br/>日時：${created}<br />`;
+    case 3:
+      return `[課題に移動(WEBページに遷移)](${create_link(body)})<br/>件名：${summary}<br/>コメント：${comment.content}<br/>日時：${created}`;
+    case 4:
+      return `[課題に移動(WEBページに遷移)](${create_link(body)})<br/>日時：${created}`;
+    case 6:
+      return `[課題に移動(WEBページに遷移)](${create_link(body)})<br/>件名：${summary}<br/>課題の詳細：${description}<br/>日時：${created}<br />`;
+    // case 14:
+    //   return `[info][title]${createdUser.name}が課題をまとめて更新しました。[/title]${create_links(link).join("\n")}[/info]`;
+    case 17:
+      return `[課題に移動(WEBページに遷移)](${create_link(body)})<br/>件名：${summary}<br/>課題の詳細：${comment.content}<br/>日時：${created}`;
+  }
+  return `unknown event type: ${type}`;
 }
 
-// 課題削除時のメッセージ作成
-const issue_delete = (teams_title, bl_body) => ({ "title": teams_title, "text": `日時：${bl_body.created}` });
+const create_link = body => {
+  const { key_id, comment } = body.content;
+  let link = `${BACKLOG_URI}view/${body.project.projectKey}-${key_id}`;
+  if (comment && comment.id) {
+    link += "#comment-" + comment.id;
+  }
+  return link;
+}
+
+const create_links = links => {
+  const urls = [];
+  links.forEach(link => {
+    urls.push(create_link(link));
+  });
+  return urls;
+}
 
 // 課題複数更新時のメッセージ作成
-const issue_multi_update = (teams_title, bl_body) => {
+const issue_multi_update = body => {
   const 
-    bl_created = bl_body.created,
-    testt = bl_body.content.link.length;
+    created = body.created,
+    test = body.content.link?.length,
+    teams_title = create_title(body);
 
-  let teams_text = "";
-  for (let i = 0; i < testt; i++) {
-      console.log(bl_body.content.link[i].title);
-      teams_text += `件名：${bl_body.content.link[i].title}<br/>`;
+  let teams_text = create_links(body.content.link).join("<br/>");
+  for (let i = 0; i < test; i++) {
+      teams_text += `件名：${body.content.link[i].title}<br/>`;
   }
   
-  teams_text += `日時：${bl_created}`;
+  teams_text += `日時：${created}<br/>`;
 
   // Teamsへの通知本文
   const payload_dic = {
@@ -90,18 +106,7 @@ const issue_multi_update = (teams_title, bl_body) => {
   return payload_dic;
 }
 
-// コメントお知らせ追加時のメッセージ作成
-const issue_comment_notice_add = (teams_title, bl_body) => {
-  const 
-    bl_summary = bl_body.content.summary,
-    bl_comment = bl_body.content.comment.content,
-    bl_created = bl_body.created;
-  
-  let  teams_text = `件名：${bl_summary}<br/>課題の詳細：${bl_comment}<br/>日時：${bl_created}`;
-  // Teamsへの通知本文作成
-  const payload_dic = {
-      "title": teams_title,
-      "text": teams_text,
-  }
-  return payload_dic; 
-}
+const handleIssue = body => ({
+  "title": create_title(body),
+  "text": create_text(body),
+});
